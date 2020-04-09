1625c91aa908d803d00ad7409376b980fa1f4287
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2020-04-09 07:43:23 +0300

    chapter 2 parser ex. 2

---
+++ chapter-2/parser/excercises/ex_2.hs
@@ -0,0 +1,54 @@
+module Main where
+import Control.Monad
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+main :: IO ()
+main = do args <- getArgs
+          putStrLn (readExpr $ args !! 0)
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]  
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (escapedSymbol <|> noneOf "\"")
+                 char '"'
+                 return $ String x
+
+escapedSymbol :: Parser Char
+escapedSymbol = do char '\\'
+                   escapedChar <- char '\"'
+                   return escapedChar
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = [first] ++ rest
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          otherwise -> Atom atom
+    
+parseNumber :: Parser LispVal
+parseNumber = (many1 digit) >>= (\x -> return $ (Number . read) x)
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
